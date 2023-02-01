d0fdfd0602cba540c93fa653c670c77db15c3a90
Jake Baek <jake.baek@soulfx.com>
2023-02-01 18:45:09 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, TouchableOpacity, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+      <TouchableOpacity>
+        <Image fadeDuration={1000} source={{uri: 'https://picsum.photos/200/300', width: 200, height: 300}}/>
+      </TouchableOpacity>
+      <Button title='Click Me' onPress={() => console.log('hi')}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fefae0',
+    paddingTop: 16
   },
 });
